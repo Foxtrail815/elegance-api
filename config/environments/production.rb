8e5b48df5fe21a2257a7f469c2271884ac9ca3e0
Nick <lennin20@hotmail.com>
2018-08-10 12:16:41 -0500

    Added Sengrid Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'https://elegance-api-app.herokuapp.com/', :protocol => 'https'}
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
